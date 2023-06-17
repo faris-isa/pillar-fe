c40a7ba5c545d5f27fa69d2d58ba326b5fd24341
faris-isa <m.faris.isa@gmail.com>
2023-06-17 09:08:54 +0700

    update detail for fifo

--- src/routes/(app)/fifo/+page.server.ts
+++ src/routes/(app)/fifo/+page.server.ts
@@ -1,9 +1,20 @@
 import type { PageServerLoad } from "./$types"
 import config from "$lib/config.json"
+import { fail, type Actions, type RequestEvent } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ cookies, locals, parent }) => {
     await parent()
 	const token: any = cookies.get('token');
+
+	const fetchOption = async () => {
+		try {
+			const res = await (await fetch(config.domain + "/inventory", {headers: {token : token}, mode: "cors", credentials: "include"})).json();
+			const data = res.message == "OK" ? res.data : [];
+			return data;
+		} catch (error) {
+			return [];
+		}
+    }
 
 	const fetchData = async () => {
 		try {
@@ -17,6 +28,46 @@ export const load: PageServerLoad = async ({ cookies, locals, parent }) => {
     
 	return {
 		user: locals.user,
+		option: fetchOption(),
 		inventory: fetchData()
 	}
+}
+
+export const actions: Actions = {
+    fifoSubmit:async ({ request, cookies }: RequestEvent) => {
+		const token: any = cookies.get('token');
+
+        const dataForm = await request.formData();
+        const kode_barang = dataForm.get('valueBarang');    
+
+        if(!kode_barang){
+            return fail(403, {
+                error: true,
+                message: 'Please, form required  !',
+                kode_barang
+            })
+        }
+
+        let res = await (await fetch(config.domain + "/inventory/fifo", {
+            headers: {
+                token: token, 
+                'Content-Type': 'application/json'
+            }, mode: "cors", method: "POST",
+            body: JSON.stringify({
+				kode_barang
+            })
+        })).json()
+        const statusCode  = res.statusCode;
+		if (statusCode > 200){
+			return fail(statusCode, {
+				error: true,
+				message: res.message,
+				kode_barang
+			})
+		}
+
+		const data = res.message == "OK" ? res.data[0] : [];
+		return data;
+        
+    }
 }
--- src/routes/(app)/fifo/+page.svelte
+++ src/routes/(app)/fifo/+page.svelte
@@ -1,16 +1,47 @@
 <script lang="ts">
+    import { applyAction, enhance } from "$app/forms";
 	import loading from "$lib/loading";
 	import { onMount } from "svelte";
     import { writable } from "svelte/store";
+    import Select from 'svelte-select';
+    import PlusCircle from "svelte-material-icons/PlusCircle.svelte";
+    import MinusCircle from "svelte-material-icons/MinusCircle.svelte";
 
-    export let data;
-    let { inventory } = data;
+    export let data: any;
+    export let form: any;
+    let { option, inventory } = data;
 
     const inventoryData = writable(inventory);
+    interface selectOption {
+        value: string;
+        label: string;
+    }
+
+    const collections: selectOption[] = [];
+
+    let selected: null = null, removeMode = false, addMode = false;
 
     function normalizeDate(date: Date) {
         const normalizedDateTime = new Date(date).toLocaleString('id-ID');
         return normalizedDateTime;
+    }
+
+    option.forEach((item: { kode_barang: string }) => {
+        const a1: selectOption = {
+            value: item.kode_barang,
+            label: item.kode_barang,
+        };
+        collections.push(a1);
+    });
+
+    const updateTable = (data: any) => {
+        inventoryData.update(item => {
+            const filtered = item.map((item2: { details: any[] }) => {
+                const details = item2.details.filter(detail => detail.id !== data.id);
+                return { ...item2, details };
+            });
+            return filtered;
+        });
     }
 
     onMount(()=>{
@@ -20,19 +51,95 @@
 
 <div class="h-screen flex-col pt-5 pr-6 pb-6 pl-3">
     <div class="card-min h-full">
-        <div class="p-5 flex">
+        <div class="p-5 flex justify-between">
             <div class="text-lg">
                 FIFO (First In First Out) &emsp;
             </div>
-            <div class="h-7 w-[2px] bg-gray-300"></div>          
+            {#if !addMode && !removeMode }    
+            <button class="pl-5" on:click={(e) => {addMode = true, removeMode = false}}>
+                <span  class="inline-block align-middle -mt-1">
+                    <PlusCircle color="green" height={18} width={18}/>
+                </span>
+                add Item
+            </button>
+            {/if}
+            {#if !removeMode && !addMode }    
+            <button class="pl-5" on:click={(e) => {removeMode = true, addMode = false}}>
+                <span  class="inline-block align-middle -mt-1">
+                    <MinusCircle color="red" height={18} width={18}/>
+                </span>
+                remove Item
+            </button>
+            {/if}
+            {#if removeMode}
+            <button class="pl-5" on:click={(e) => {removeMode = false}}>
+                <span  class="inline-block align-middle -mt-1">
+                    <PlusCircle color="green" height={18} width={18}/>
+                </span>
+                cancel remove Item
+            </button>
+            {/if}
+            {#if addMode}
+            <button class="pl-5" on:click={(e) => {addMode = false}}>
+                <span  class="inline-block align-middle -mt-1">
+                    <MinusCircle color="red" height={18} width={18}/>
+                </span>
+                cancel add Item
+            </button>
+            {/if}
         </div>
-        <div class="h-[calc(100%-10rem)] overflow-auto">
+        <div class="h-[calc(100%-8rem)] overflow-auto">
+            <div class="{removeMode ? "flex" : "hidden"} w-[calc(100%-2.5rem)] my-5 mx-5">
+                <div class="{(form?.error) ? "flex" : "hidden"} bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-4" role="alert">
+                    <strong class="font-bold">Failed !</strong>
+                    <span class="block sm:inline ml-2">{form?.message}</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <!-- svelte-ignore a11y-click-events-have-key-events -->
+                        <!-- svelte-ignore a11y-interactive-supports-focus -->
+                        <svg on:click={() =>{ form.error = false }} class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                    </span>
+                </div>
+                <form action="?/fifoSubmit" method="post" use:enhance={({form, data}) => {
+                    // Before form submission to server
+                    const dataForm = data
+    
+                    // After form submission
+                    return async ({ result, update }) => {
+                        if(result.type === 'success'){
+                            const data = result.data;
+                            updateTable(data);
+                            form.reset();
+                        }
+    
+                        if(result.type === 'failure'){
+                            await applyAction(result)
+                        }
+
+                        update();
+                    }
+                }}>
+
+                    <div class="w-full grid grid-cols-2 gap-4">
+                        <div class="">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
+                            <label class="block" id="kode_barang">Kode Barang<span class="text-red-400 text-xl mb-2">*</span><label>
+                            <input type="hidden" name="valueBarang" bind:value={selected}>
+                            <Select containerStyles="margin-top: 0.5rem;" bind:justValue={selected} name="kode_barang" items={collections} placeholder="Pilih kode barang !" class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                        </div>
+                        <div class="flex items-baseline mb-5">
+                            <button type="submit" class="mt-8 px-10 py-3 text-white bg-red-600 rounded-lg hover:bg-red-900">OUT !</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
             <table class="w-[calc(100%-2.5rem)] text-center mx-5">
                 <thead class="">
-                    <tr>
-                        <td class="pb-2 pt-5 bg-white sticky top-0 w-[20%]">Kode Barang</td>
-                        <td class="pb-2 pt-5 bg-white sticky top-0 w-[30%]">Serial Number</td>
-                        <td class="pb-2 pt-5 bg-white sticky top-0">Created Date</td>
+                    <tr class="border-2 border-black">
+                        <td class="pb-4 pt-4 bg-white sticky top-0 w-[20%] border-2 border-black">Kode Barang</td>
+                        <td class="pb-4 pt-4 bg-white sticky top-0 w-[30%] border-2 border-black">Serial Number</td>
+                        <td class="pb-4 pt-4 bg-white sticky top-0 w-1/5">Created Date</td>
+                        <td class="pb-4 pt-4 bg-white sticky top-0">Action</td>
                     </tr>
                 </thead>
                 <tbody>
@@ -40,28 +147,29 @@
                         {#if inventData.details.length >= 1}
                             {#each inventData.details as detailed, j}
                                 {#if j === 0}
-                                    <tr>
-                                        <td class="py-3" rowspan="{inventData.details.length}">{inventData.kode_barang}</td>
-                                        <td class="py-3">{detailed.serial_number}</td>
-                                        <td class="py-3">{normalizeDate(detailed.created_at)}</td>
+                                    <tr class="border border-gray-200">
+                                        <td class="py-3 border-r-2" rowspan="{inventData.details.length}">{inventData.kode_barang}</td>
+                                        <td class="py-3 border-r-2 text-sm">{detailed.serial_number}</td>
+                                        <td class="py-3 border-r-2">{normalizeDate(detailed.created_at)}</td>
                                     </tr>
                                 {:else}
-                                    <tr>
-                                        <td class="py-3">{detailed.serial_number}</td>
-                                        <td class="py-3">{normalizeDate(detailed.created_at)}</td>
+                                    <tr class="border border-gray-200">
+                                        <td class="py-3 border-r-2 text-sm">{detailed.serial_number}</td>
+                                        <td class="py-3 border-r-2">{normalizeDate(detailed.created_at)}</td>
                                     </tr>
                                 {/if}
                             {/each}
                         {:else}
-                            <tr>
-                                <td class="py-3">{inventData.kode_barang}</td>
-                                <td class="py-3">-</td>
-                                <td class="py-3">-</td>
+                            <tr class="border border-gray-200">
+                                <td class="py-3 border-r-2">{inventData.kode_barang}</td>
+                                <td class="py-3 border-r-2">-</td>
+                                <td class="py-3 border-r-2">-</td>
                             </tr>
                         {/if}
                     {/each}
                 </tbody>
             </table>
+
         </div>
         <div class="sticky w-full bottom-0 h-16 bg-white flex justify-center items-center gap-5">
             <div>1</div>
